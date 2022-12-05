866846bd102f7892271da5ac8aa2b36978c98676
김민수 <alstnalsgud@gmail.com>
2022-12-05 22:18:22 +0900

    #15 fix config properties (#16)
    설정값들 객체로 변환하여 관리

--- src/main/kotlin/com/example/authserver/config/WebConfig.kt
+++ src/main/kotlin/com/example/authserver/config/WebConfig.kt
@@ -2,6 +2,7 @@ package com.example.authserver.config
 
 import com.example.authserver.exception.InvalidTokenException
 import com.example.authserver.jwt.AuthToken
+import com.example.authserver.properties.WebProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
@@ -14,11 +15,13 @@ import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 
 @Configuration
-class WebConfig : WebFluxConfigurer{
+class WebConfig(
+    private val webProperties: WebProperties
+) : WebFluxConfigurer{
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins(webProperties.originHost)
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .maxAge(3600)
     }
